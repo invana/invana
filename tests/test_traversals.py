ae6c49f8dc94c09e53907b7a26cb1159b85f4a43
RAVIRAJA-PC\rrmerugu <rrmerugu@gmail.com>
2022-01-09 20:25:45 +0530

    tests updated for search and create traversals

--- tests/test_traversals.py
+++ tests/test_traversals.py
@@ -1,4 +1,5 @@
 from invana_py.connector.connector import GremlinConnector
+from invana_py.serializer.structure import RelationShip, Node
 
 
 class TestSearchTraversal:
@@ -20,3 +21,20 @@ class TestSearchTraversal:
                                          has__name__startingWith="invana"
                                          ).elementMap().toList()
         assert result.__len__() == 2
+
+
+class TestCreateTraversal:
+
+    def test_vertex_create(self, connection: GremlinConnector):
+        result = connection.g.create_vertex("NewLabel", name="Hi Label").next()
+        result = connection.g.V().search(has__id=result.id).elementMap().next()
+        assert isinstance(result, Node)
+        assert result.properties.name == "Hi Label"
+
+    def test_edge_create(self, connection: GremlinConnector):
+        node1 = connection.g.create_vertex("NewLabel", name="Hi Label 1").next()
+        node2 = connection.g.create_vertex("NewLabel", name="Hi Label 2").next()
+        connection.g.create_edge("has_link", node1.id, node2.id, name="Hi Edge 1").next()
+        result = connection.g.E().search(has__name="Hi Edge 1").elementMap().next()
+        assert isinstance(result, RelationShip)
+        assert result.properties.name == "Hi Edge 1"
