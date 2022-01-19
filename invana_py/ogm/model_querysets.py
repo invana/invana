9ce28e107a419a46793b31f0eb12d64c0a961c09
Ravi Raja Merugu <rrmerugu@gmail.com>
2022-01-19 13:12:50 +0530

    copyright statement added

--- invana_py/ogm/model_querysets.py
+++ invana_py/ogm/model_querysets.py
@@ -1,3 +1,17 @@
+#    Copyright 2021 Invana
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#     http:www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
 from .decorators import dont_allow_has_label_kwargs, serialize_to_model_datatypes, validate_kwargs_for_create, \
     validate_kwargs_for_search, add_has_label_kwargs_from_model
 from .exceptions import FieldNotFoundError, FieldValidationError
