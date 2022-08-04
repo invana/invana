c3f08084a73e126253ba0a5b316e7db8b06852d3
Ravi Raja Merugu <rrmerugu@gmail.com>
2022-08-05 00:41:04 +0530

    chores(examples) - deleting extras

--- examples/import_large_data.py
+++
@@ -1,9 +0,0 @@
-from invana.connector.connector import GremlinConnector
-
-gremlin_url = "ws://megamind-ws:8182/gremlin"
-connector = GremlinConnector(gremlin_url)
-
-for i in range(0, 300):
-    connector.g.addV("TestUser").property("name", f"Ravi {i}").next()
-
-connector.close()
