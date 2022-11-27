39c1418492154222b922a9a3d23636a8d33a86cd
Ravi Raja Merugu <rrmeurgu@gmail.com>
2022-11-27 16:40:43 +0530

    get_vertex_properties_of_edges helper method added to get the vertex properties of the vertices in edges

---
+++ invana/helpers.py
@@ -0,0 +1,26 @@
+from invana.serializer.element_structure import RelationShip, Node
+
+
+def get_vertex_properties_of_edges(edges, graph):
+    """
+    By default, edge json will not have inv and outv properties,
+    this method will fetch and stitch the fill vertex details to the edge inv and outv
+    """
+
+    vertex_ids = []
+    for edge in edges:
+        if not isinstance(edge, RelationShip):
+            raise Exception("relationship data should be RelationShip type")
+        vertex_ids.append(edge.inv.id)
+        vertex_ids.append(edge.outv.id)
+    unique_vertex_ids = list(set(vertex_ids))
+    vertex_instances = graph.vertex.search(
+        has__id__within=unique_vertex_ids).to_list()
+
+    vertices_dict = dict([(v.id, v) for v in vertex_instances])
+    for edge in edges:
+        edge.inv_back = edge.inv
+        edge.inv = vertices_dict[edge.inv.id]
+        edge.outv_back = edge.outv
+        edge.outv = vertices_dict[edge.outv.id]
+    return edges
