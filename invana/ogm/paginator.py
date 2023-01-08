255d83bc51f3742dbfcd95cf0327d86a3658eda3
Ravi Raja Merugu <rrmerugu@gmail.com>
2023-01-09 00:00:06 +0530

    ogm paginator fixes

--- invana/ogm/paginator.py
+++ invana/ogm/paginator.py
@@ -13,19 +13,19 @@
 #     limitations under the License.
 
 import copy
-from .querysets import QuerySetResultSet
+from ..gremlin.resultsets import GremlinQueryResultSet
 from .utils import copy_traversal
 
 
 class QuerySetPaginator:
 
-    def __init__(self, queryset_result: QuerySetResultSet, page_size: int):
+    def __init__(self, queryset_result: GremlinQueryResultSet, page_size: int):
         self.queryset_result = queryset_result
         self.bytecode = copy.deepcopy(queryset_result.get_traversal().bytecode)
         self.page_size = page_size
 
-    def page(self, page_no) -> QuerySetResultSet:
+    def page(self, page_no) -> GremlinQueryResultSet:
         traversal = copy_traversal(self.queryset_result.get_traversal())
         range_set = ((page_no - 1) * self.page_size), (page_no * self.page_size)
         _ = traversal.range(*range_set)
-        return QuerySetResultSet(_)
+        return GremlinQueryResultSet(_)
