31243961ffc752d9910b9268673fde5ca14732f9
Ravi Raja Merugu <rrmerugu@gmail.com>
2023-03-24 18:38:23 +0530

    cypher fixes for get or create class

--- invana_connectors/cypher/querysets/nodes.py
+++ invana_connectors/cypher/querysets/nodes.py
@@ -2,15 +2,20 @@ from invana_connectors.querysets import NodeQuerySetBase
 from neomodel.contrib import SemiStructuredNode
 from neomodel import ( config, exceptions )
 from invana_connectors.cypher.decorators import serialize_neomodel_to_invana_objects
-import importlib
+from neomodel.core import db
+
+def get_or_create_class(label_name):
+
+    label_set = frozenset([label_name,])
+    if label_set in db._NODE_CLASS_REGISTRY:
+        return db._NODE_CLASS_REGISTRY[label_set]
+    label_cls = type(label_name, (SemiStructuredNode,), {}) 
+    return label_cls
 
 class NodeCypherQuerySet(NodeQuerySetBase):
 
     @serialize_neomodel_to_invana_objects
     def create(self, label, **properties):        
-        try:
-            label_cls = type(label, (SemiStructuredNode,), {}) 
-        except exceptions.NodeClassAlreadyDefined:
-            label_cls = i = getattr(importlib.import_module('neomodel.core'), label)
+        label_cls = get_or_create_class(label)
         return label_cls(**properties).save()
  
