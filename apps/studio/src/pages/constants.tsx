280b76fd7b1b977078cea6fb772b497d04e1c56c
Ravi Raja Merugu <rrmerugu@gmail.com>
2025-01-25 15:39:08 +0530

    old/stalled plugins data removed

--- apps/studio/src/pages/constants.tsx
+++ apps/studio/src/pages/constants.tsx
@@ -1,16 +1,16 @@
 import { SideBarNavitemProps } from "@invana/ui";
-import { CircleHelp, Compass, Database, Network, Package } from "lucide-react";
+import { Activity, CircleHelp, Compass, MonitorCog, Network, Package } from "lucide-react";
 
 
 export const LogoComponent = <Package className="h-5 w-5 text-foreground" />
-
 
 export const sideBarTopNavitems: SideBarNavitemProps[] = [
   // { name: "Home", href: "/home", icon: Home },
   { name: "Explorer", href: "/explorer", icon: Compass },
   { name: "Modeller", href: "/modeller", icon: Network },
-  { name: "Data Management", href: "/connections", icon: Database },
-  // { name: "Activity", href: "#", icon: Clock },
+  // { name: "Data Management", href: "/connections", icon: Database },
+  { name: "Activity History", href: "#", icon: Activity },
+  { name: "Display Settings", href: "#", icon: MonitorCog },
 ]
 
 export const sideBarBottomNavitems: SideBarNavitemProps[] = [
