ac03e6170ff927976dce170257b4200f7333250e
Ravi Raja Merugu <rrmerugu@gmail.com>
2025-01-26 16:14:10 +0530

    form builder components added

--- packages/ui/stories/form-builder/form-builder.stories.tsx
+++ packages/ui/stories/form-builder/form-builder.stories.tsx
@@ -5,7 +5,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Graph Components/FormBuilder',
+  title: 'Form Builder/FormBuilder',
   component: FormBuilder,
   parameters: {
     layout: 'centered',
