7fbb12a561ffa4d140c1391223e3396ae8c2c1ec
Ravi Raja Merugu <rrmerugu@gmail.com>
2025-01-26 17:10:19 +0530

    examples updates

---
+++ packages/ui/stories/form-builder/basic-form-builder.stories.tsx
@@ -0,0 +1,27 @@
+import { FormGenerator } from '@/form-generator/form-generator';
+import type { Meta, StoryObj } from '@storybook/react';
+
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Form Builder/Form Builder',
+  component: FormGenerator,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: {
+
+  },
+} satisfies Meta<typeof FormGenerator>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default: Story = {
+  args: {
+    className: 'w-[300px]',
+  },
+};
