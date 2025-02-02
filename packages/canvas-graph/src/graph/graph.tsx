1f4bd1f8867afcde7b58e2288609b2ed7e032290
Ravi Raja Merugu <rrmerugu@gmail.com>
2025-02-03 03:56:09 +0530

    style cleanups

--- packages/canvas-graph/src/graph/graph.tsx
+++ packages/canvas-graph/src/graph/graph.tsx
@@ -67,9 +67,6 @@ export const CanvasGraph: React.FC<CanvasGraphProps> = forwardRef((props, ref) =
   return (
     <div style={props?.style || {}} className={'graph-canvas overflow-hidden ' + props.className || ''}>
       {graph && header && <CanvasToolBar getGraph={() => graph} />}
-
-
-
       <MemoizedGraphin
         ref={localRef}
         onReady={(graph) => {
