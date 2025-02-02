35f1f13270920687d9bd5a9b252563f8b6939ad7
Ravi Raja Merugu <rrmerugu@gmail.com>
2025-02-03 00:34:32 +0530

    button size styling fixes

--- packages/ui/src/components/ui/button.tsx
+++ packages/ui/src/components/ui/button.tsx
@@ -25,8 +25,8 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-9 px-4 py-2",
-        sm: "h-6 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
+        sm: "h-6 rounded-sm px-3 text-sm",
+        lg: "h-10 rounded-sm px-8",
         icon: "h-9 w-9",
         "icon-sm": "h-7 w-7",
       },
