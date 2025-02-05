a4f410b6f96f6ccab3765b4fa841f12de71c9b71
Ravi Raja Merugu <rrmerugu@gmail.com>
2025-02-05 16:31:50 +0530

    styling updates

--- packages/canvas-graph/src/manager/styling.ts
+++ packages/canvas-graph/src/manager/styling.ts
@@ -3,40 +3,75 @@ import { DEFAULT_EDGE_STYLE, DEFAULT_NODE_STYLE } from '../options/elements';
 import { CanvasManagerOptions, ICanvasStyleOptions } from './types';
 import { convert_canvas_style_to_g6_style, convert_edge_canvas_style_to_g6_sytle, convert_node_canvas_style_to_g6_style } from './utils';
 import { defaultEdgeStyle, defaultNodeStyle } from './defaults';
-import { ICanvasStyle } from '@invana/data-store';
+import { CanvasNodeStyle, ICanvasStyle, mergeDeep } from '@invana/data-store';
+import { NodeStyle } from '@antv/g6/lib/spec/element/node';
+import { EdgeStyle } from '@antv/g6/lib/spec/element/edge';
 
 
 export class GraphStyle {
 
-  graph!: Graph
-  options!: CanvasManagerOptions
+  private graph!: Graph
+  private options!: CanvasManagerOptions
 
   constructor(graph: Graph, options: CanvasManagerOptions) {
     console.log("GraphStyle.constructor", graph, options.styles);
     this.graph = graph;
     this.options = options;
-    this.init();
+    this.updateDefaults(this.options);
   }
 
-  init() {
+
+
+  private getUpdatedDefaultNodeStyle = (options: CanvasManagerOptions, theme: string): NodeStyle => {
+    return convert_node_canvas_style_to_g6_style(
+      options?.styles?.defaultNode ?? {},
+      theme as string
+    )
+  }
+
+  private getUpdatedDefaultEdgeStyle = (options: CanvasManagerOptions, theme: string): EdgeStyle => {
+    return convert_edge_canvas_style_to_g6_sytle(
+      options?.styles?.defaultEdge ?? {},
+      theme as string
+    );
+  }
+
+  private updateOptions = (options: CanvasManagerOptions) => {
+    this.options = options;
+  }
+
+
+  updateDefaults(newOptions: CanvasManagerOptions) {
+
+    // update existing options with the new options 
+    const options: CanvasManagerOptions = mergeDeep(this.options, newOptions);
+
+    let graphOptions: GraphOptions = {}
+
     // canvas styling
-    const canvasStyle = convert_canvas_style_to_g6_style(this.options?.styles?.canvas as ICanvasStyle ?? {});
-    const options: GraphOptions = { ...canvasStyle };
+    if (newOptions.styles?.canvas) {
+      const canvasStyle = convert_canvas_style_to_g6_style(options?.styles?.canvas as ICanvasStyle ?? {});
+      graphOptions = { ...canvasStyle, ...graphOptions };
+    }
+
     // node styling
-    const defaultNodeStyle = convert_node_canvas_style_to_g6_style(
-      this.options?.styles?.defaultNode ?? {},
-      canvasStyle.theme as string
-    );
-    options.node = defaultNodeStyle as NodeOptions;
+    if (newOptions.styles?.defaultNode) {
+      const defaultNodeStyle = this.getUpdatedDefaultNodeStyle(options, options?.styles?.canvas?.theme as string);
+      graphOptions.node = defaultNodeStyle as NodeOptions
+    }
+
+
     // edge styling
-    const defaulEdgeStyle = convert_edge_canvas_style_to_g6_sytle(
-      this.options?.styles?.defaultEdge ?? {},
-      canvasStyle.theme as string
-    );
-    options.edge = defaulEdgeStyle as EdgeOptions
-    // update graph options
-    console.log("finalOptions", options)
-    this.graph.setOptions(options)
+    if (newOptions.styles?.defaultEdge) {
+      const defaulEdgeStyle = this.getUpdatedDefaultEdgeStyle(options, options?.styles?.canvas?.theme as string);
+      graphOptions.edge = defaulEdgeStyle as EdgeOptions
+    }
+
+    // update location options variable
+    this.updateOptions(options);
+    // update graph graphOptions
+    console.log("graphOptions", graphOptions)
+    this.graph.setOptions(graphOptions)
   }
 
   defaultNodeStyleBasedOnTheme = (theme: ThemeOptions) => {
@@ -80,14 +115,15 @@ export class GraphStyle {
   }
 
   setTheme(theme: ThemeOptions) {
-    const nodeStyle = this.defaultNodeStyleBasedOnTheme(theme);
-    const edgeStyle = this.defaultEdgeStyleBasedOnTheme(theme);
+    // const nodeStyle = this.defaultNodeStyleBasedOnTheme(theme);
+    // const edgeStyle = this.defaultEdgeStyleBasedOnTheme(theme);
 
-    this.graph.setOptions({
-      theme,
-      node: nodeStyle,
-      edge: edgeStyle
-    })
+    // this.graph.setOptions({
+    //   theme,
+    //   node: nodeStyle,
+    //   edge: edgeStyle
+    // })
+    this.updateDefaults()
     // this.graph.setTheme(theme)
     // // update node styling
     // this.graph.setNode(nodeStyle);
