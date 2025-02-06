ef20f5e5716a75e44dffb3458dfb77635275d957
Ravi Raja Merugu <rrmerugu@gmail.com>
2025-02-06 11:40:06 +0530

    explorer2 page moved to explorer

--- apps/studio/src/pages/explorer/explorer.tsx
+++ apps/studio/src/pages/explorer/explorer.tsx
@@ -1,169 +1,259 @@
-import React, { useRef, useState } from 'react';
-import { LogoComponent, bottomNavItems } from '../constants';
-import { ProductCopyRightInfo, ProductName } from '@/constants';
-import {
-  BlankLayout,
-  LeftNavItem
-} from '@invana/ui';
-import { ReactFlowProvider } from '@invana/canvas-flow';
-import { AppHeader, AppFooter, AppMain } from '@invana/ui/themes/app'
-// import useTheme from '@invana/ui/hooks/useTheme';
-import AppHeaderRight from '@/ui/header/app-header-right';
-import { CanvasGraph, CanvasToolBar, defaultOptions } from '@invana/canvas-graph';
+
+import { LeftNavItem, useThemeStore } from '@invana/ui';
+import { DefaultLayout } from '@invana/ui/themes/default/default';
+import { useDefaultLayoutStore } from '@invana/ui/themes/default/store';
+import { Activity, Compass, MonitorCog, Network, Terminal } from 'lucide-react';
+import { Button } from '@invana/ui';
+import { useState, useRef, useEffect } from 'react';
+import { ProductName } from '@/constants';
+import { CanvasGraph } from '@invana/canvas-graph';
 import { flightData } from '@invana/example-datasets'
-import { Activity, MonitorCog, Network, SearchIcon, Terminal } from 'lucide-react'
-import { Graph } from '@antv/g6';
+import AppHeaderRight from '@/ui/header/app-header-right';
 import { QueryForm } from '@/ui/forms/query-form';
-import useLayout from '@/hooks/useLayout'
+import { PanelContent } from '@invana/ui/components/theme/panel-content';
+import { ActivityHistoryView } from '@/ui/components/activity-history';
+import { CanvasManagerOptions } from '@invana/canvas-graph/manager/types';
+import {
+  CANVAS_CONTEXT_MENU_BEHAVIOR, CLICK_SELECT_BEHAVIOR, DRAG_CANVAS_BEHAVIOR,
+  DRAG_ELEMENT_BEHAVIOR, EDGE_CONTEXT_MENU_BEHAVIOR, EDGE_TOOLTIP_BEHAVIOR,
+  HOVER_ACTIVATE_BEHAVIOR, LASSO_SELECT_BEHAVIOR, NODE_CONTEXT_MENU_BEHAVIOR,
+  NODE_TOOLTIP_BEHAVIOR, PROPERTY_VIEWER_BEHAVIOR, ZOOM_CANVAS_BEHAVIOR
+} from '@invana/canvas-graph/defaults/behaviors';
+import { MAP_NODE_SIZE_TRANSFORMER } from '@invana/canvas-graph/defaults/transforms';
+import { MINIMAP_PLUGIN, HISTORY_PLUGIN } from '@invana/canvas-graph/defaults/plugins';
+import { GRID_LAYOUT } from '@invana/canvas-graph/defaults/layouts';
+import { ExtensionCategory, register } from '@antv/g6';
+import { EdgeTooltipBehavior, NodeTooltipBehavior, PropertyViewerBehavior } from '@invana/canvas-graph/behaviours';
+import { NodeContextMenuBehavior } from '@invana/canvas-graph/behaviours/context-menus/node';
+import { EdgeContextMenuBehavior } from '@invana/canvas-graph/behaviours/context-menus/edge';
+import { CanvasContextMenuBehavior } from '@invana/canvas-graph/behaviours/context-menus/canvas';
+import { CanvasManager } from '@invana/canvas-graph/manager';
+import { DEFAULT_STYLE_OPTIONS } from '@invana/canvas-graph/manager/defaults';
+import { CanvasToolBar } from '@invana/canvas-graph/plugins';
+
+
+register(ExtensionCategory.BEHAVIOR, 'tooltip-node', NodeTooltipBehavior, true);
+register(ExtensionCategory.BEHAVIOR, 'tooltip-edge', EdgeTooltipBehavior, true);
+register(ExtensionCategory.BEHAVIOR, 'node-context-menu', NodeContextMenuBehavior, true);
+register(ExtensionCategory.BEHAVIOR, 'edge-context-menu', EdgeContextMenuBehavior, true);
+register(ExtensionCategory.BEHAVIOR, 'canvas-context-menu', CanvasContextMenuBehavior, true);
+register(ExtensionCategory.BEHAVIOR, 'property-viewer', PropertyViewerBehavior, true);
+
+
+const defaultOptions: CanvasManagerOptions = {
+  behaviors: [
+    DRAG_CANVAS_BEHAVIOR,
+    ZOOM_CANVAS_BEHAVIOR,
+    DRAG_ELEMENT_BEHAVIOR,
+    HOVER_ACTIVATE_BEHAVIOR,
+    CLICK_SELECT_BEHAVIOR,
+    LASSO_SELECT_BEHAVIOR,
+    NODE_TOOLTIP_BEHAVIOR,
+    EDGE_TOOLTIP_BEHAVIOR,
+    NODE_CONTEXT_MENU_BEHAVIOR,
+    EDGE_CONTEXT_MENU_BEHAVIOR,
+    CANVAS_CONTEXT_MENU_BEHAVIOR,
+    {
+      ...PROPERTY_VIEWER_BEHAVIOR,
+      className: 'top-[44px] right-[0px] w-[320px] h-[calc(100vh-72px)]'
+    }
+  ],
+  transforms: [
+    MAP_NODE_SIZE_TRANSFORMER
+  ],
+  plugins: [
+    MINIMAP_PLUGIN,
+    HISTORY_PLUGIN,
+    // GRID_PLUGIN
+  ],
+  layout: GRID_LAYOUT,
+  styles: DEFAULT_STYLE_OPTIONS
+}
 
 
 const ExplorerPage: React.FC = () => {
 
-  // const { theme } = useTheme();
-  // const [data, setData] = React.useState({ nodes: [], edges: [] });
-  // const [canvasManager, setGraphManager] = React.useState<CanvasManager | null>(null);
-  // const initGraphManager = React.useCallback((manager: CanvasManager) => {
-  //   setGraphManager(manager);
-  // }, []);
-
-
-
-
-  const { leftSidebar, setLeftSidebar, rightSidebar, setRightSidebar } = useLayout()
 
 
   const [isReady, setIsReady] = useState(false);
-  const containerRef = useRef<{ getGraph: () => Graph } | null>(null);
-  const graphManagerRef = useRef(null);
+  const graphManagerRef = useRef<CanvasManager | null>(null);
+  // const canvasGraphRef = useRef<{
+  //   getGraph: () => Graph
+  //   getGraphManager: () => CanvasManager
+  // } | null>(null);
+  // const graphManagerRef = useRef(null);
+
+  const { theme, } = useThemeStore()
+
+
+  const {
+    leftContentName,
+    setLeftContentName,
+    bottomContentName,
+    toggleLeftContent,
+    toggleBottomContent,
+  } = useDefaultLayoutStore()
 
 
 
   const topNavItems: LeftNavItem[] = [
     {
-      name: "Search",
-      key: "search",
+      icon: Network,
+      name: "Model",
+      key: "model",
       onClick: () => {
-        console.log("SearchIcon clicked", leftSidebar)
-        return leftSidebar === undefined ? setLeftSidebar("search") : setLeftSidebar(undefined)
+        return toggleLeftContent("model")
       },
-      icon: SearchIcon
     },
     {
       name: "Query",
       key: "query",
       onClick: () => {
-        console.log("SearchIcon clicked", rightSidebar)
-        return rightSidebar === undefined ? setRightSidebar("search") : setRightSidebar(undefined)
+        return toggleLeftContent("query")
       },
       icon: Terminal
     },
-    { name: "Modeller", key: 'modeller', href: "/modeller", icon: Network },
+    // {
+    //   icon: Book,
+    //   key: "documentation",
+    //   name: "documentation",
+    //   onClick: () => {
+    //     console.log("Clicked:", "Documentation")
+    //   }
+    // },
+    // { name: "Modeller", key: 'modeller', href: "/modeller", icon: Network },
     // { name: "Data Management", href: "/connections", icon: Database },
-    { name: "Activity History", key: 'activity-history', href: "#", icon: Activity },
+    {
+      name: "Activity History",
+      key: 'activity-history',
+      onClick: () => {
+        return toggleLeftContent("activity-history")
+      },
+      icon: Activity
+    },
     { name: "Display Settings", key: 'display-settings', href: "#", icon: MonitorCog },
+
   ]
-
-
-
-  // useEffect(() => {
-  //   // Initialize canvasManager here and set it to graphManagerRef.current
-  //   // Example:
-  //   // graphManagerRef.current = new CanvasManager();
-  //   setIsReady(true);
-  // }, []);
-
-
-
-  // const canvasManager = new CanvasManager(null);
-
-  // const [graph, setGraph] = React.useState<Graph>(null);
-
-  // React.useEffect(() => {
-  //   runQuery()
-  // }, []);
-
-  // const runQuery = () => {
-  //   const randInt = Math.floor(Math.random() * 10) + 1;
-
-  //   fetchGraphQLData(`g.V().limit(${randInt}).toList()`).then(d => {
-  //     const response = serializeToGraph(d.data);
-  //     console.log("response", response);
-  //     setData(response);
-  //   });
-
-  // }
-
-  // console.log("===data2", data)
-
 
   const options = { ...defaultOptions }
 
-  console.log("ExplorerPage leftSidebar", leftSidebar, leftSidebar ? 25 : 0)
-  return (
-    <BlankLayout
-      logo={LogoComponent}
-      topNavItems={topNavItems}
-      bottomNavItems={bottomNavItems}
-    >
-      <ReactFlowProvider fitView>
-        <AppHeader
-          left={
-            <>
-              {/* <Compass className='h-4 w-4' /> */}
-              <span className='font-bold mr-2'>{ProductName}</span>
-              <span className='mr-2'>|</span>
-              <span>Explorer</span>
-            </>
-          }
-          center={
-            <>
-              {isReady && containerRef.current && <CanvasToolBar getGraph={containerRef.current.getGraph} />}
-            </>
-          }
-          right={
-            <>
-              <AppHeaderRight />
-            </>
-          }
-        >
-        </AppHeader>
-        <AppMain>
+  // if (options.behaviors) {
+  //   options.behaviors = options.behaviors.filter(b => b !== 'property-viewer');
 
-          {
-            leftSidebar && <div className={"w-[480px]  flex h-[calc(100vh-80px)] z-[1000]   overflow-hidden absolute"}>
+  //   options.behaviors.push({
+  //     key: 'property-viewer',
+  //     type: 'property-viewer',
+  //     className: 'top-[44px] right-[0px] w-[320px] h-[calc(100vh-72px)]',
+  //   });
+  // }
 
-              <QueryForm />
+
+  // useEffect(() => {
+  //   console.log("mainTopContentSize or leftContentSize updated ", mainTopContentSize, leftContentSize)
+  //   const graph = canvasGraphRef.current?.getGraph();
+  //   if (graph && isReady) {
+
+  //     graph.resize();
+  //     graph.layout();
+  //     graph.render();
+  //     graph.fitView();
+  //   }
+  // }, [mainTopContentSize, leftContentSize, isReady]);
+
+
+  useEffect(() => {
+    if (graphManagerRef.current && isReady) {
+      // const canvasManager = canvasGraphRef.current.getGraphManager();
+      console.log("getUpdatedStylingOptions, theme", theme)
+      graphManagerRef.current.setTheme(theme)
+    }
+  }, [theme, isReady]);
+
+  return <DefaultLayout
+    headerProps={{
+      left: (
+        <>
+          {/* <span className='ml-3'><Compass className='w-5 h-5' /></span> */}
+          <span className='font-bold mr-2 ml-3'>{ProductName}</span>
+          <span className='mr-2'>|</span>
+          <span>Explorer</span>
+        </>
+      ),
+      center: (
+        <>
+          {isReady && graphManagerRef.current && <CanvasToolBar getGraph={graphManagerRef.current.getGraph} />}
+        </>
+      ),
+      right: (
+        <>
+          <AppHeaderRight />
+        </>
+      )
+    }}
+
+    leftNavProps={{
+      topNavItems: topNavItems,
+    }}
+    leftContent={
+      <div className="space-y-2 ">
+        {leftContentName === "model" &&
+          <PanelContent title={"Model"} onClose={() => setLeftContentName(undefined)} showClose>
+            <div className='h-full px-3 py-2'>
+              <p >Graph model comes here</p>
             </div>
-          }
+          </PanelContent>
+        }
+        {leftContentName === "query" &&
+          <PanelContent title={"Query Console"} onClose={() => setLeftContentName(undefined)} showClose>
+            <QueryForm className=' ' />
+          </PanelContent>
+        }
+        {leftContentName === "activity-history" &&
+          <PanelContent title={"Activity History"} onClose={() => setLeftContentName(undefined)} showClose>
+            <ActivityHistoryView className='p-3 mb-3 h-[calc(100vh-80px)] ' />
+          </PanelContent>
+        }
+        {leftContentName === "display-settings" &&
+          <PanelContent title={"Display Settings"} onClose={() => setLeftContentName(undefined)} showClose>
+            <p>Display Settings here </p>
+          </PanelContent>
+        }
+      </div>
+    }
+    mainTopContent={
 
-          {
-            rightSidebar && <div className={"w-[320px] right-0 flex h-[calc(100vh-80px)] z-[1000]   overflow-hidden absolute"}>
-              <QueryForm />
-            </div>
-          }
-          <CanvasGraph
-            ref={containerRef}
-            style={{ width: "100%", height: "100%" }}
-            className={"bg-background"}
-            //@ts-expect-error
-            canvasManager={graphManagerRef.current}
-            initialData={flightData}
-            onReady={() => {
-              console.log("onReady")
-              setIsReady(true)
-            }}
-            options={options}
-          />
-        </AppMain>
+      // <div className="flex h-full items-center justify-center ">
 
-        <AppFooter
-          right={ProductCopyRightInfo}
-        >
+      <CanvasGraph
+        // ref={canvasGraphRef}
+        containerStyle={{ width: "100%", height: "100%" }}
+        className={"bg-background"}
+        initData={flightData}
+        onReady={(graphManager: CanvasManager) => {
+          console.log("onReady", graphManager)
+          // graphManagerRef.current = graphManager;
+          setIsReady(true)
+        }}
+        options={options}
+      />
 
-        </AppFooter>
-      </ReactFlowProvider>
-    </BlankLayout >
-  );
-};
+      // </div>
+    }
+    mainBottomContent={
+      <>
+        <Button size={"sm"} onClick={() => toggleBottomContent('query')}>Toggle Bottom {bottomContentName}</Button>
+      </>
+    }
+    rightContent={
+      <></>
+    }
+
+
+
+
+  />
+
+
+}
 
 export default ExplorerPage;
