9b5285177b75fcec13af077920fee6fbb6f4ba0f
Ravi Raja Merugu <rrmerugu@gmail.com>
2025-02-11 01:14:11 +0530

    canvasManager.getGraphSchema() added to get the schema based on data

--- packages/data-store/src/graph/graph.ts
+++ packages/data-store/src/graph/graph.ts
@@ -1,7 +1,115 @@
+import { IEdgeSchema, IGraphSchema, INodeSchema, ISchemaProperty } from '../types/schema';
 import { GraphDataCRUD } from './crud';
 
 
 export class GraphStore extends GraphDataCRUD {
+
+  private nodeSchemas: Map<string, Map<string, ISchemaProperty>> = new Map();
+
+  // private nodeSchemas: Map<string, INodeSchema> = new Map();
+  private edgeSchemas: Map<string, IEdgeSchema> = new Map();
+
+  // createNodeSchema(nodeSchema: INodeSchema) {
+  //   this.nodeSchemas.set(nodeSchema.name, nodeSchema);
+  // }
+
+  // createEdgeSchema(edgeSchema: IEdgeSchema) {
+  //   this.edgeSchemas.set(edgeSchema.name, edgeSchema);
+  // }
+
+  // getNodeSchema(label: string): INodeSchema | undefined {
+  //   return this.nodeSchemas.get(label);
+  // }
+
+  // getEdgeSchema(label: string): IEdgeSchema | undefined {
+  //   return this.edgeSchemas.get(label);
+  // }
+
+  generateSchema(): IGraphSchema {
+
+    const graph = this.data;
+    // Iterate through all nodes once
+    graph.forEachNode((_, attributes) => {
+      if (!attributes.type) return; // Skip nodes without a type
+      console.log("attributes", attributes);
+
+      const nodeProperties = attributes.properties || {};
+
+      if (!this.nodeSchemas.has(attributes.type)) {
+        this.nodeSchemas.set(attributes.type, new Map());
+      }
+
+      const schema = this.nodeSchemas.get(attributes.type)!;
+
+      Object.keys(nodeProperties || {}).forEach((key) => {
+        if (key === 'type') return;
+
+        if (!schema.has(key)) {
+          schema.set(key, {
+            name: key,
+            type: typeof nodeProperties[key],
+            required: true,
+            defaultValue: null,
+            description: '',
+          });
+        }
+      });
+    });
+
+    // Iterate through all edges once
+    graph.forEachEdge((edge, attributes, source, target) => {
+      if (!attributes.type) return; // Skip edges without a type
+      const edgeProperties = attributes.properties || {};
+
+      if (!this.edgeSchemas.has(attributes.type)) {
+        this.edgeSchemas.set(attributes.type, {
+          name: attributes.type,
+          properties: [],
+          source,
+          target,
+          isDirected: graph.isDirected(edge),
+        });
+      }
+
+      const schema = this.edgeSchemas.get(attributes.type)!;
+      Object.keys(edgeProperties).forEach((key) => {
+        if (key === 'type') return;
+
+        if (!schema.properties.some((p) => p.name === key)) {
+          schema.properties.push({
+            name: key,
+            type: typeof edgeProperties[key],
+            required: true,
+            defaultValue: null,
+            description: '',
+          });
+        }
+      });
+    });
+    return this.getGraphSchema();
+  }
+
+  // Get schema for the entire graph
+  getGraphSchema(): IGraphSchema {
+    return {
+      nodes: Array.from(this.nodeSchemas.entries()).map(([name, properties]) => ({
+        name,
+        properties: Array.from(properties.values()),
+      })),
+      edges: Array.from(this.edgeSchemas.values()),
+    };
+  }
+
+  // getSchemaForNode(nodeType: string): INodeSchema {
+  //   const properties = this.nodeSchemas.get(nodeType);
+  //   if (!properties) {
+  //     throw new Error(`Node schema not found for type ${nodeType}`);
+  //   }
+  //   return {
+  //     name: nodeType,
+  //     properties: Array.from(properties.values()),
+  //   };
+  // }
 
 }
 
---
+++ packages/example-datasets/datasets/usersData.ts
@@ -0,0 +1,42 @@
+
+import { ICanvasData, ICanvasEdge, ICanvasNode } from "@invana/data-store";
+
+
+export const usersDataSet: ICanvasData = {
+  nodes: [
+    {
+      id: "user1",
+      label: "Alice",
+      properties: { name: "Alice", age: 30 },
+      type: "User",
+    },
+    {
+      id: "user2",
+      label: "Bob",
+      properties: { name: "Bob" },
+      type: "User",
+    },
+    {
+      id: "post1",
+      label: "Graphology Guide",
+      properties: { title: "Graphology Guide", likes: 100 },
+      type: "Post",
+    },
+  ] as ICanvasNode[],
+  edges: [
+    {
+      id: "user1->user2",
+      source: "user1",
+      target: "user2",
+      type: "Follows",
+      properties: { since: 2022 },
+    },
+    {
+      id: "user2->post1",
+      source: "user2",
+      target: "post1",
+      type: "Likes",
+      properties: { weight: 5 },
+    },
+  ] as ICanvasEdge[],
+};
