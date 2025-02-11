3fc63b21b13fac96fc4ee8c0f0151109a19167ad
Ravi Raja Merugu <rrmerugu@gmail.com>
2025-02-11 17:45:02 +0530

    layout cleanups

--- packages/ui/src/themes/layout-v2/layout.tsx
+++ packages/ui/src/themes/layout-v2/layout.tsx
@@ -87,39 +87,20 @@ export const DefaultV2Layout: React.FC<DefaultV2LayoutProps> = ({
                   </ResizablePanelGroup>
                 </ResizablePanel>
 
-                <ResizableHandle
-                  withHandle
-                  className={cn("transition-opacity duration-300", !rightContentName && "hidden")}
-                />
-                <ResizablePanel
-                  defaultSize={rightContentSize}
-                  minSize={15}
-                  maxSize={45}
-                  style={{
-                    display: rightContentName ? "block" : "none",
-                  }}
-                >
-                  {/* <ResizablePanelGroup
-                    direction="vertical"
-                    autoSaveId="right-persistence"
-                  >
-                    <ResizablePanel minSize={30} defaultSize={mainTopContentSize} className="overflow-y-auto" > */}
-                  {/* <div className="flex h-full items-center justify-center"> */}
-                  {rightContent}
-                  {/* </div> */}
-                  {/* </ResizablePanel>
-                    <ResizableHandle withHandle className={cn("transition-opacity duration-300")} />
-                    <ResizablePanel minSize={3} defaultSize={100 - mainTopContentSize}>
-                      <div className="flex-1 flex-col h-full">
-
-                      </div>
+                {rightContentName &&
+                  <>
+                    <ResizableHandle
+                      withHandle
+                      className={cn("transition-opacity duration-300")} />
+                    <ResizablePanel
+                      defaultSize={rightContentSize}
+                      minSize={15}
+                      maxSize={45}
+                    >
+                      {rightContent}
                     </ResizablePanel>
-                  </ResizablePanelGroup> */}
-
-                  {/* <PanelContent title="Navigation Tree" onClose={() => toggleLeftContent('query')} showClose> */}
-                  {/* {rightContent} */}
-                  {/* </PanelContent> */}
-                </ResizablePanel>
+                  </>
+                }
 
               </ResizablePanelGroup>
             </div>
