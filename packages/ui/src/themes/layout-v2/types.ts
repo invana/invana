ad385b0089610a9a85452e8610b5cea4912242a3
Ravi Raja Merugu <rrmerugu@gmail.com>
2025-02-11 14:54:10 +0530

    layout cleanups  for right nav items

--- packages/ui/src/themes/layout-v2/types.ts
+++ packages/ui/src/themes/layout-v2/types.ts
@@ -14,6 +14,7 @@ import { LeftNavProps } from "./left-nav"
 export interface DefaultV2LayoutProps {
   className?: string
   leftNavProps: LeftNavProps
+  rightNavProps: LeftNavProps
   headerProps: AppHeaderProps
 
 
