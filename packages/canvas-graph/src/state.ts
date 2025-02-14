8643db2966501b474c46875a296f36efcb4663fd
Ravi Raja Merugu <rrmerugu@gmail.com>
2025-02-15 03:48:20 +0530

    graphin like component created as CanvasGraph

---
+++ packages/canvas-graph/src/state.ts
@@ -0,0 +1,18 @@
+
+
+
+/*
+
+data - graph data 
+
+styling - canvas, defaultNode, defaultEdge, customise by types 
+
+interactions - drag elements, drag canvas, select elements, select - click / lasso  , zoom, pan, hover
+
+transformations - node size by centrality, edge size by weight, process parallel edges
+
+plugins - context menus, tooltips, background patterns - grid, dotted, history, minimap,
+
+layout - 
+
+*/
