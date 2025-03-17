ca34e5bd3e7913f9f97796cf0ac9394841d251fc
Ravi Raja Merugu <rrmerugu@gmail.com>
2025-03-17 20:44:39 +0530

    generic node data added

--- packages/canvas-reactflow/stories/node-templates/GenericNode/index.stories.ts
+++ packages/canvas-reactflow/stories/node-templates/GenericNode/index.stories.ts
@@ -1,7 +1,46 @@
 import { CanvasFlow } from '../../../src/app/app';
 import type { Meta, StoryObj } from '@storybook/react';
 import { data } from "./data";
+import nodesData from '../../example-data/all-nodes.json';
+import edgesData from '../../example-data/all-relationships.json';
+import DagreLayoutEngine from '../../dagre-layout';
+import { MarkerType } from "@xyflow/react";
 
+
+
+
+const nodes = nodesData.map((n) => {
+  return {
+    id: n['Entity_ID:ID'],
+    type: 'GenericNode',
+    data: {
+      type: n[':Label'],
+      label: n['name']
+    }
+  }
+})
+
+const edges = edgesData.map((e) => {
+  return {
+    id: `${e[':START_ID']}-${e[':END_ID']}`,
+    source: e[':START_ID'],
+    target: e[':END_ID'],
+    type: 'smoothstep',
+    data: {
+      type: e[':TYPE'],
+      timestamp: e['timestamp']
+    },
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+    },
+  }
+})
+
+console.log("---------- nodes", nodes)
+
+const layoutEngine = new DagreLayoutEngine()
+
+const { layoutedNodes, layoutedEdges } = layoutEngine.getLayoutedElements(nodes, edges)
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
@@ -26,5 +65,12 @@ export const GenericNode: Story = {
     nodes: data.nodes,
     edges: data.edges,
     layoutDirection: "TB"
+  },
+};
+
+export const GenericNode2: Story = {
+  args: {
+    nodes: layoutedNodes,
+    edges: layoutedEdges,
   },
 };
